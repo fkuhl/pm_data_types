9586635962e2b07cd03d2570e84d7306d5a0ec2b
Leone, Mark A [Axios] <mleone@lgsinnovations.com>
2020-04-14 16:18:36 -0400

    Use prettyprint, and print json representation

--- try_data.py
+++ try_data.py
@@ -3,6 +3,10 @@ from pm_data_types.address import Address
 from pm_data_types.household import Household
 from datetime import date
 import jsonpickle
+import json
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
 
 mem1 = Member()
 mem1.family_name = "Hornswoggle"
@@ -12,15 +16,15 @@ trans1 = Transaction()
 trans1.date = date(1999, 1, 1)
 trans1.type = TransactionType.BIRTH
 mem1.transactions = [trans1]
-print(f"mem1 is {mem1.member_id}")
+pp.pprint(f"mem1 is {mem1.member_id}")
 mem1.member_id = ""
-print(f"mem1 is {mem1.member_id}")
+pp.pprint(f"mem1 is {mem1.member_id}")
 
 house = Household()
 house.id = "xyz"
 house.head = mem1
 
 pickled = jsonpickle.encode(house)
-print(pickled)
+pp.pprint(json.loads(pickled))
 recons = jsonpickle.decode(pickled)
-print(recons.head.family_name)
+pp.pprint(recons.head.family_name)
